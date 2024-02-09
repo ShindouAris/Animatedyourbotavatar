329a4c6f9318fb449639e8040bb7767b93a1c367
Kamisato Kasadin <tranthanhnam.ccc@gmail.com>
2024-02-09 12:14:38 +0700

    command prefix to slash command + fix get env

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import disnake
 from disnake.ext import commands
 import asyncio
 import os
-
+import dotenv
+dotenv.load_dotenv()
 intents = disnake.Intents.all()
 
 client = commands.Bot("a?", intents=intents)
@@ -12,12 +13,14 @@ async def on_ready():
     print(f"loggin as {client.user}")
 
 
-@client.command()
-async def change_avt(self):
+@client.slash_command()
+async def setup_user(self, ctx: disnake.AppCommandInteraction):
+    await ctx.response.defer()
 
     try: # Thay thế tệp trong ./data thành avt bạn thích (Nhớ đổi tên thành `avt`)
         with open('./data/avt.gif', 'rb') as f:
                 await client.user.edit(avatar=f.read())
+                await ctx.edit_original_response("Đã thiết lập thành công")
                 print("Done")
                 exit(1)
     except Exception as e:
